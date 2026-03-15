0205a90b14ad236771830a5f214e1781a64091e7
Nil <s952106200213@gmail.com>
2026-03-15 22:30:29 +0800

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/json-diff/',
+  base: '/json-diff-studio/',
   plugins: [react()],
 });
